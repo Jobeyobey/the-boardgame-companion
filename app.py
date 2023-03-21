284d5bd50eaf4b67de81d45e74b02ecebbce0451
Jobeyobey <117482381+Jobeyobey@users.noreply.github.com>
2023-03-21 23:43:28 +0000

    Created gamepage.html. Anchor tags on search link to gamepage.html and fetch relevant data

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def hello():
+def index():
   # GET
   if request.method == "GET":
     return render_template("index.html")
@@ -14,6 +14,13 @@ def hello():
     query = request.form.get("query")
 
     return render_template("index.html", query=query)
+  
+@app.route("/gamepage")
+def gamepage():
+  # Get requested game id from URL query
+  id = request.args['id']
+
+  return render_template("gamepage.html", id=id)
 
 if __name__ == "__main__":
   app.run()
