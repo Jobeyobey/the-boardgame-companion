46e5f17118572be10d060d95fb0d0cebdeca2db1
Jobeyobey <117482381+Jobeyobey@users.noreply.github.com>
2023-03-21 23:10:31 +0000

    Search and list games. Each game result links to a BGG page

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello():
-  return render_template("index.html")
+  # GET
+  if request.method == "GET":
+    return render_template("index.html")
+
+  # POST
+  if request.method == "POST":
+    # Get user query
+    query = request.form.get("query")
+
+    return render_template("index.html", query=query)
 
 if __name__ == "__main__":
   app.run()
